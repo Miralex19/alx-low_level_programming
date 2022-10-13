2d884e6ff393fe71ff4a62604d213786b3464fdb
Miracle Ohuka <miracleohuka12@gmail.com>
2022-10-13 09:34:11 -0700

    jj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,10 @@
 
 /**
   * main - Printing
-  *
   * Return: Always 0 (Successful)
   */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual
-			puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
