3e5fdbb6a5ea61ce3f451356e64f88e9c2f3c166
Miracle Ohuka <miracleohuka12@gmail.com>
2022-10-13 08:43:42 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 /**
 *  main - prints "text"
 *Return: 0
 */
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle\n"
+	char Mystr[] = "Programming is like building a multilingual puzzle"
 
 	puts(Mystr);
 	return (0);
