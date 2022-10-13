3534718f1aea9d55d21d78314904ebac01e6380c
Miracle Ohuka <miracleohuka12@gmail.com>
2022-10-13 08:17:03 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+*  main - prints "text"
+*Return: 0
+*/
+int main(void)
+{
+	char Mystr[] = "Programming is like building a multilingual puzzle\n"
+
+	puts(Mystr);
+	return (0);
+}
+
