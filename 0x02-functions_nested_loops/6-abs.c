791469ff68e3ba14ca5a649cb4f986fb5967b5fb
Miracle Ohuka <miracleohuka12@gmail.com>
2022-10-18 23:25:27 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_abs - checks absolute value of integer
+ *@int: integer input
+ *Return: n or -n
+ *
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
