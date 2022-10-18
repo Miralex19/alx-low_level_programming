fe34138dd3088598ac85fa70ec6b5ad61e1b5c98
Miracle Ohuka <miracleohuka12@gmail.com>
2022-10-18 09:34:50 -0700

    og

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,27 +7,19 @@
  *
  */
 
-
 void print_alphabet_x10(void)
 {
-	char c, i;
-
-	for (i = 0; i <= 9; i++)
+int count = 0;
+	while (count < 10)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		char ch = 'a';
+
+		while (char ch <= 'z')
 		{
-			_putchar(c);
+			_putchar(ch);
+			ch++;
 		}
+		count++;
 		_putchar('\n');
 	}
 }
-/**
- * main - entry point
- *
- * Return: 0 (passed)
- */
-int main(void)
-{
-print_alphabet_x10();
-}
-
