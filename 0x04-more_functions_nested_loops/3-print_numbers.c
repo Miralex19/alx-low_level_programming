0e571b156e597fed37ac1a9243c0501510f4fc98
Miracle Ohuka <miracleohuka12@gmail.com>
2022-10-20 22:32:35 +0000

    first

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_numbers - print  from 0 -9
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
