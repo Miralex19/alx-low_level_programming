3b68dacd37c4d69cff1ca0f0181749a343f6f36c
Miracle Ohuka <miracleohuka12@gmail.com>
2022-11-07 14:48:27 +0000

    tasss

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,16 +1,15 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-* create_array - creates an array of chars.
-*@size: size of the array
-*@c: stored character
-*
-*Return: pointer of an array of characters
-*/
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: storaged char
+ *
+ * Return: pointer of an array of chars
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *cr;
-
 	unsigned int i;
 
 	if (size == 0)
@@ -20,13 +19,9 @@ char *create_array(unsigned int size, char c)
 
 	if (cr == NULL)
 		return (NULL);
-	int i = 0;
 
-	while (i < size)
-	{
+	for (i = 0; i < size; i++)
 		cr[i] = c;
-		i++;
-	}
-	return (cr);
 
+	return (cr);
 }
